43ab8885fc7b60a44c3849f288ffb46e4715a179
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-09-10 00:40:59 -0400

    refactor: add types, add basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(
+        `
+            <div>
+                <h1>Hello</h1>
+            </div>
+        `
+    );
+});
+
+app.listen(3000, () => {
+    console.log('app listening!')
+});
